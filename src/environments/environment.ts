aca73459b36ff76fdc54b619bdb5acea145c1979
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2021-06-07 02:39:26 +0530

    update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,20 +5,7 @@
 export const environment = {
   production: false,
   Route: 'https://safe-mountain-83585.herokuapp.com',
-  Twitter_API_Key: 'bocSzKt0jf6yzLUdH3XRfRqwP',
-  Twitter_API_Key_secret: 'y2J9bSJBTJL8DkmZTR5tV5yVtFjxZTE3wbcgiXa40ULafomKtM',
-  Twitter_Bearer_Token: 'AAAAAAAAAAAAAAAAAAAAAM1kQQEAAAAAU3abxXvxi0xx5TVKF62QVzMWi0s%3DCa19e3xm1JWbBCUQEtrT7JJnH8uH5oyeGQcLvWD84fCtzh8Scn',
-  Twitter_Access_Token: '90111923-CApxWGAUbSlgHD5KrcnpUL9tDOEqsbCNWNHUiFf3q',
-  Twitter_Access_Secret: 'cZ2cwAKiXdryciPWfQxKkUdSR1MBqrAHiTnVGqeVwfGEl',
-  firebaseConfig: {
-    apiKey: "AIzaSyBMhsmMWCuDx8ZD6jhASnLbcgdhRQaLkH4",
-    authDomain: "http-start-15d34.firebaseapp.com",
-    databaseURL: "https://http-start-15d34.firebaseio.com",
-    projectId: "http-start-15d34",
-    storageBucket: "http-start-15d34.appspot.com",
-    messagingSenderId: "735182522830",
-    appId: "1:735182522830:web:1bd8b5b16784f50bfac39f"
-  }
+
 };
 
 /*
