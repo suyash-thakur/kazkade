badc11e9ed95a68fd3999059ca5a70c32581dbcb
suyash-thakur <47927368+suyash-thakur@users.noreply.github.com>
2021-06-17 10:10:49 +0530

    updates

--- src/app/news/news.component.ts
+++ src/app/news/news.component.ts
@@ -10,7 +10,7 @@ export class NewsComponent implements OnInit {
   call = false;
   news = [];
   constructor( private http: HttpClient) {
-    this.http.get('https://newsapi.org/v2/everything?q=bitcoin&apiKey=2edf6f125baf49c9a1e4224351124306').subscribe((res: any) => {
+    this.http.get('https://newsapi.org/v2/everything?q=crypto&apiKey=2edf6f125baf49c9a1e4224351124306').subscribe((res: any) => {
       console.log(res);
       this.news = res.articles;
       this.call = true;
